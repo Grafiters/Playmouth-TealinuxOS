277499639e3f2a3559bdfe6124f39d227b3f4202
Grafiters <bayugrafit@gmail.com>
2019-05-04 01:39:26 +0700

    edited setup for installation plymouth

--- sample/setup.sh
+++ sample/setup.sh
@@ -7,42 +7,35 @@ echo "|       refrenced from Abas_        |"
 echo "|------------------------------------"
 
 echo ""
-echo ""
 
 echo "|--------------------------"
 echo "| creating new plymouth...|"
 echo "|--------------------------"
 sudo mkdir /usr/share/plymouth/themes/steavia-tea
-echo "~"
+echo "selesai"
 
-
-echo ""
 echo ""
 echo "|--------------------------"
 echo "| copying asset...        |"
 echo "|--------------------------"
 sudo cp steavia-tea-rendered/* /usr/share/plymouth/themes/steavia-tea/
 sudo cp script/* /usr/share/plymouth/themes/steavia-tea/
-echo "~"
+echo "selesai"
 
-
-echo ""
 echo ""
 echo "|--------------------------"
 echo "| Backup default.plymouth |"
 echo "|--------------------------"
 sudo mv /etc/alternatives/default.plymouth /etc/alternatives/default.plymouth.bak
-echo "~"
+echo "selesai"
 
-echo ""
 echo ""
 echo "|--------------------------"
 echo "| Installing New Config   |"
 echo "|--------------------------"
 sudo cp default.plymouth /etc/alternatives/default.plymouth
-echo "~"
+echo "selesai"
 
-echo ""
 echo ""
 echo "|---------------------------------"
 echo "| All Done, to test the plymouth |"
